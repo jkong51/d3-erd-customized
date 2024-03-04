486f0d8abee64000b0b4c3e54923c20417f62d7f
JK <jasonlekong@hotmail.com>
2024-03-04 11:42:49 +0800

    initial changes to accomodate new library

--- js/api.js
+++ js/api.js
@@ -1,9 +1,9 @@
 
 
-function  saveChanges (d) {
-    var data = (d.entity_id).concat("-").concat(d.x/width).concat("-").concat(d.y/height)
+function saveChanges(d) {
+    var data = (d.entity_id).concat("-").concat(d.x / width).concat("-").concat(d.y / height)
 
-    let url = "http://localhost:8888/d3-erd/erd_api.php?data="+data;
+    let url = "http://localhost:8888/d3-erd/erd_api.php?data=" + data;
 
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", url, true);
@@ -16,35 +16,42 @@ function  saveChanges (d) {
     }
     return xhttp.send();
 }
-function getJsonData(callback){
 
-    if(!callback){
-        d3.json('data/sample_data.json',function(data){
+function getJsonData(callback) {
+
+    if (!callback) {
+        d3.json('data/sample_camstar_data.json').then((data) => {
             load(data);
-        })
+        });
+
+        // OLD
+        // d3.json('data/sample_camstar_data.json', function (data) {
+        //     load(data);
+        // })
     }
 
-    let url = "http://localhost:8888/d3-erd/erd_api.php?v=dev"
+    // Dont need to see here yet
+    // let url = "http://localhost:8888/d3-erd/erd_api.php?v=dev"
 
-    var xhttp = new XMLHttpRequest();
+    // var xhttp = new XMLHttpRequest();
 
-    xhttp.open("GET", url, true);
-    xhttp.onreadystatechange = function () {//Call a function when the state changes.
+    // xhttp.open("GET", url, true);
+    // xhttp.onreadystatechange = function () {//Call a function when the state changes.
 
-        if (this.readyState === 4 && this.status === 200 ) {
-            var rt = xhttp.responseText;
-            let jData = JSON.parse(xhttp.responseText)
-            console.log(jData)
-            if(callback) {load(jData)}
-        }
-    }
-    xhttp.send();
+    //     if (this.readyState === 4 && this.status === 200) {
+    //         var rt = xhttp.responseText;
+    //         let jData = JSON.parse(xhttp.responseText)
+    //         console.log(jData)
+    //         if (callback) { load(jData) }
+    //     }
+    // }
+    // xhttp.send();
 }
 
 // call getJsonData(callback) to use the api to retrieve the ER data from a database
 // otherwise load the local json sample data
 
-setTimeout(function(){
+setTimeout(function () {
     getJsonData() // loads data/sample_data.json
     //getJsonData(load) // load data from database
-},100)
+}, 100)
